341c54ec636a5aaf81ad5258f0edf11af7c28d8a
Pontus Persson <pom@skurt.com>
2017-06-24 22:05:39 +0200

    Export proper

---
+++ index.js
@@ -0,0 +1,18 @@
+const Game = require('./Game');
+const InputPlayer = require('./InputPlayer');
+const InputRecorder = require('./InputRecorder');
+const Scene = require('./Scene');
+
+const XMLLoader = require('./loader/XML');
+const Mouse = require('./Mouse');
+const Hud = require('./Hud');
+
+module.exports = {
+    Game,
+    InputPlayer,
+    InputRecorder,
+    XMLLoader,
+    Mouse,
+    Hud,
+    Scene,
+};
