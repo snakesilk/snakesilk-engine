4ac071e241a3e79748ed610116cc5f5e365c3e3f
Pontus Persson <pontus.alexander@gmail.com>
2017-06-25 22:05:40 +0200

    Add test for main export.

---
+++ src/__tests__/index-test.js
@@ -0,0 +1,23 @@
+const expect = require('expect.js');
+const sinon = require('sinon');
+const packageJSON = require('../../package.json');
+const Main = require('../index.js');
+
+describe('Main Export', function() {
+  it('is defined in package.json', () => {
+    expect(packageJSON.main).to.be('./src/index.js');
+  });
+
+  [
+    ['Game', '../Game'],
+    ['InputPlayer', '../InputPlayer'],
+    ['InputRecorder', '../InputRecorder'],
+    ['Mouse', '../Mouse'],
+    ['Hud', '../Hud'],
+    ['Scene', '../Scene'],
+  ].forEach(([module, target]) => {
+    it(`exposes ${module}`, () => {
+      expect(Main[module]).to.be(require(target));
+    });
+  });
+});
---
+++ src/index.js
@@ -0,0 +1,16 @@
+const Game = require('./Game');
+const InputPlayer = require('./InputPlayer');
+const InputRecorder = require('./InputRecorder');
+const Scene = require('./Scene');
+
+const Mouse = require('./Mouse');
+const Hud = require('./Hud');
+
+module.exports = {
+    Game,
+    InputPlayer,
+    InputRecorder,
+    Mouse,
+    Hud,
+    Scene,
+};
