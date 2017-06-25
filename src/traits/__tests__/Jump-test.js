94322c42664a20810f1d1b4f3479ffa6bdfe02c2
Pontus Persson <pontus.alexander@gmail.com>
2017-06-25 21:13:08 +0200

    Cleanup unit tests.
    * Put in `__tests__` subfolder of unit.

---
+++ src/traits/__tests__/Jump-test.js
@@ -0,0 +1,41 @@
+const expect = require('expect.js');
+const sinon = require('sinon');
+
+const Entity = require('../../Entity');
+const World = require('../../World');
+const Physics = require('../Physics');
+const Jump = require('../Jump');
+const Solid = require('../Solid');
+
+describe('Jump', function() {
+  it.skip('should maintain jump height despite variations in time', function() {
+    const step = 1/120;
+
+    const world = new World();
+
+    const ground = new Entity();
+    ground.applyTrait(new Solid());
+    ground.addCollisionRect(1000, 10);
+    ground.position.set(0, 0);
+
+    const jumper = new Character();
+    jumper.addCollisionRect(10, 10);
+    jumper.position.set(0, 30);
+    jumper.applyTrait(new Solid());
+    jumper.applyTrait(new Physics());
+    jumper.physics.mass = 1;
+    jumper.applyTrait(new Jump());
+    jumper.jump.force.x = 10000;
+
+    world.addObject(jumper);
+    world.addObject(ground);
+
+    let maxLoops = 40;
+    expect(jumper.jump._ready).to.be(false);
+    while (jumper.jump._ready === false && maxLoops--) {
+      world.updateTime(step);
+    }
+    expect(jumper.jump._ready).to.be(true);
+    expect(jumper.position.y).to.equal(10);
+  });
+});
