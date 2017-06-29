25872dbf40d54d844eea89f29627e7538d692646
Pontus Persson <pontus.alexander@gmail.com>
2017-06-29 10:15:38 +0200

    Rearrange index test.

--- src/__tests__/index-test.js
+++ src/__tests__/index-test.js
@@ -10,78 +10,78 @@ describe('Main Export', function() {
     expect(packageJSON.main).to.be('./dist/index.js');
   });
 
-  [
-    ['AI', '../AI'],
-    ['Animation', '../Animation'],
-    ['Camera', '../Camera'],
-    ['Game', '../Game'],
-    ['Entity', '../Entity'],
-    ['Events', '../Events'],
-    ['Loader', '../Loader'],
-    ['Scene', '../Scene'],
-    ['Tween', '../Tween'],
-    ['World', '../World'],
+  describe('Exports', () => {
+    [
+      ['AI', '../AI'],
+      ['Animation', '../Animation'],
+      ['Camera', '../Camera'],
+      ['Game', '../Game'],
+      ['Entity', '../Entity'],
+      ['Events', '../Events'],
+      ['Loader', '../Loader'],
+      ['Scene', '../Scene'],
+      ['Tween', '../Tween'],
+      ['World', '../World'],
 
-    ['UVAnimator', '../animator/UV'],
-    ['UVCoords', '../UVCoords'],
+      ['UVAnimator', '../animator/UV'],
+      ['UVCoords', '../UVCoords'],
 
-    ['BitmapFont', '../BitmapFont'],
-    ['InputPlayer', '../InputPlayer'],
-    ['InputRecorder', '../InputRecorder'],
-    ['SyncPromise', '../SyncPromise'],
+      ['BitmapFont', '../BitmapFont'],
+      ['InputPlayer', '../InputPlayer'],
+      ['InputRecorder', '../InputRecorder'],
+      ['SyncPromise', '../SyncPromise'],
 
-    ['Mouse', '../Mouse'],
-    ['Hud', '../Hud'],
+      ['Mouse', '../Mouse'],
+      ['Hud', '../Hud'],
 
-  ].forEach(([module, target]) => {
-    it(`exposes ${module}`, () => {
-      expect(Main[module]).to.be(require(target));
+    ].forEach(([module, target]) => {
+      it(`exposes ${module}`, () => {
+        expect(Main[module]).to.be(require(target));
+      });
     });
-  });
 
-  [
-    'Attach',
-    'Climbable',
-    'Climber',
-    'ContactDamage',
-    'Conveyor',
-    'DeathSpawn',
-    'DeathZone',
-    'Destructible',
-    'Disappearing',
-    'Door',
-    'Elevator',
-    'Environment',
-    'Fallaway',
-    'FixedForce',
-    'Glow',
-    'Headlight',
-    'Health',
-    'Invincibility',
-    'Jump',
-    'Lifetime',
-    'Light',
-    'LightControl',
-    'Move',
-    'Physics',
-    'Pickupable',
-    'Projectile',
-    'Rotate',
-    'Solid',
-    'Spawn',
-    'Stun',
-    'Teleport',
-    'Translate',
-    'Translating',
-    'Weapon',
-  ].forEach((traitName) => {
-    it(`exposes trait ${traitName}`, () => {
-      expect(new (Main.Traits[traitName])()).to.be.a(Trait);
+    [
+      'Attach',
+      'Climbable',
+      'Climber',
+      'ContactDamage',
+      'Conveyor',
+      'DeathSpawn',
+      'DeathZone',
+      'Destructible',
+      'Disappearing',
+      'Door',
+      'Elevator',
+      'Environment',
+      'Fallaway',
+      'FixedForce',
+      'Glow',
+      'Headlight',
+      'Health',
+      'Invincibility',
+      'Jump',
+      'Lifetime',
+      'Light',
+      'LightControl',
+      'Move',
+      'Physics',
+      'Pickupable',
+      'Projectile',
+      'Rotate',
+      'Solid',
+      'Spawn',
+      'Stun',
+      'Teleport',
+      'Translate',
+      'Translating',
+      'Weapon',
+    ].forEach((traitName) => {
+      it(`exposes trait ${traitName}`, () => {
+        expect(new (Main.Traits[traitName])()).to.be.a(Trait);
+      });
     });
-  });
 
-  describe('exported objects', () => {
-    it('are not available', () => {
+    it('"Objects" are not available', () => {
       expect(Main).to.not.have.property('Objects');
     });
   });
