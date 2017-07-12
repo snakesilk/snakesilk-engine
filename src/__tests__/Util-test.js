c6d1a1796a9442a27b32cf68c2712e104dfdce9b
Pontus Persson <pom@skurt.com>
2017-07-12 11:09:22 +0200

    Remove unused components.

--- src/__tests__/Util-test.js
+++ src/__tests__/Util-test.js
@@ -9,32 +9,11 @@ describe('Util', function() {
       const res = Util.renameFunction('foo', function bar() {});
       expect(res.name).to.be('foo');
     });
-
-    it('supports > 1 string length', () => {
-      const result = Util.string.fill('_!', 4);
-      expect(result).to.be('_!_!_!_!');
-    });
-
-    it('supports 0 as repeat argument', () => {
-      const result = Util.string.fill('_', 0);
-      expect(result).to.be('');
-    });
   });
 
   describe('#string.fill', () => {
-    it('returns argument #1 repeated arguement #2 times', () => {
-      const result = Util.string.fill('_', 4);
-      expect(result).to.be('____');
-    });
-
-    it('supports > 1 string length', () => {
-      const result = Util.string.fill('_!', 4);
-      expect(result).to.be('_!_!_!_!');
-    });
-
-    it('supports 0 as repeat argument', () => {
-      const result = Util.string.fill('_', 0);
-      expect(result).to.be('');
+    it('does not exist', () => {
+      expect(Util).to.not.have.property('string');
     });
   });
 });
