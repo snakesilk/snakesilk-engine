23055481d111d6dee446a4c34558f762c48aa85b
Pontus Persson <pontus.alexander@gmail.com>
2017-07-17 21:55:27 +0200

    Enable readOnly to specify configurability.

--- src/__tests__/Util-test.js
+++ src/__tests__/Util-test.js
@@ -5,15 +5,40 @@ const {readOnly} = require('../Util');
 
 describe('Util', function() {
     describe('#readOnly', () => {
-        it('creates a read-only property on an object', () => {
-            const object = {};
-            readOnly(object, {
-                'test': new Map(),
+        let object;
+
+        describe('when adding property without removable flag', () => {
+            beforeEach(() => {
+                object = {};
+                readOnly(object, {
+                    'test': new Map(),
+                });
             });
 
-            object.test.set('foo', 'bar');
-            object.test = {};
-            expect(object.test.get('foo')).to.be('bar');
+            it('creates a read-only property on an object', () => {
+                object.test.set('foo', 'bar');
+                object.test = {};
+                expect(object.test.get('foo')).to.be('bar');
+            });
+
+            it('is not removable', () => {
+                delete object.test;
+                expect(object.hasOwnProperty('test')).to.be(true);
+            });
+        });
+
+        describe('when adding property with configurable', () => {
+            beforeEach(() => {
+                object = {};
+                readOnly(object, {
+                    'test': new Map(),
+                }, true);
+            });
+
+            it('is removable', () => {
+                delete object.test;
+                expect(object.hasOwnProperty('test')).to.be(false);
+            });
         });
     });
 });
