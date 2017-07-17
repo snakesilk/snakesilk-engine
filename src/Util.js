07fe09bf29e2705ec98af3acb2559976afd9a3cd
Pontus Persson <pontus.alexander@gmail.com>
2017-07-17 21:55:27 +0200

    Enable readOnly to specify configurability.

--- src/Util.js
+++ src/Util.js
@@ -1,6 +1,7 @@
-function readOnly(object, spec) {
+function readOnly(object, spec, configurable = false) {
     Object.keys(spec).forEach(key => {
         Object.defineProperty(object, key, {
+            configurable,
             value: spec[key],
             writable: false,
         });
