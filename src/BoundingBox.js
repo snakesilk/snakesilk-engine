6dee6c4a06acce427299d04b1aa34826ca154e1f
Pontus Persson <pontus.alexander@gmail.com>
2017-07-23 15:18:59 +0200

    Refactor BoundingBox.

--- src/BoundingBox.js
+++ src/BoundingBox.js
@@ -1,8 +1,8 @@
 class BoundingBox
 {
-    constructor(hostPos, size, offset)
+    constructor(pos, size, offset)
     {
-        this.position = hostPos;
+        this.position = pos;
         this.offset = offset;
 
         this.w = size.x;
@@ -13,57 +13,54 @@ class BoundingBox
         this._w = this.w / 2;
         this._h = this.h / 2;
     }
-}
 
-Object.defineProperties(BoundingBox.prototype, {
-    x: {
-        get: function() {
-            return this.position.x + this.offset.x;
-        },
-        set: function(v) {
-            this.position.x = v - this.offset.x;
-        },
-    },
-    y: {
-        get: function() {
-            return this.position.y + this.offset.y;
-        },
-        set: function(v) {
-            this.position.y = v - this.offset.y;
-        },
-    },
-    left: {
-        get: function() {
-            return this.x - this._w;
-        },
-        set: function(v) {
-            this.x = v + this._w;
-        },
-    },
-    right: {
-        get: function() {
-            return this.x + this._w;
-        },
-        set: function(v) {
-            this.x = v - this._w;
-        },
-    },
-    top: {
-        get: function() {
-            return this.y + this._h;
-        },
-        set: function(v) {
-            this.y = v - this._h;
-        },
-    },
-    bottom: {
-        get: function() {
-            return this.y - this._h;
-        },
-        set: function(v) {
-            this.y = v + this._h;
-        },
-    },
-});
+    get x() {
+        return this.position.x + this.offset.x;
+    }
+
+    set x(v) {
+        this.position.x = v - this.offset.x;
+    }
+
+    get y() {
+        return this.position.y + this.offset.y;
+    }
+
+    set y(v) {
+        this.position.y = v - this.offset.y;
+    }
+
+    get left() {
+        return this.x - this._w;
+    }
+
+    set left(v) {
+        this.x = v + this._w;
+    }
+
+    get right() {
+        return this.x + this._w;
+    }
+
+    set right(v) {
+        this.x = v - this._w;
+    }
+
+    get top() {
+        return this.y + this._h;
+    }
+
+    set top(v) {
+        this.y = v - this._h;
+    }
+
+    get bottom() {
+        return this.y - this._h;
+    }
+
+    set bottom(v) {
+        this.y = v + this._h;
+    }
+}
 
 module.exports = BoundingBox;
