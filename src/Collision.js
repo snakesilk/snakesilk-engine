eab4b0859d55d56d7fb05d2befdeab8fb7489c28
Pontus Persson <pontus.alexander@gmail.com>
2017-07-23 18:48:21 +0200

    Refactor Collision class.

--- src/Collision.js
+++ src/Collision.js
@@ -9,7 +9,8 @@ function zonesCollide(zone1, zone2) {
         zone2.x, zone2.y, zone2.w, zone2.h);
 }
 
-class Entry {
+class Entry 
+{
     constructor(entity) {
         this.entity = entity;
         this.collidingWith = new Set();
@@ -58,10 +59,7 @@ class Collision
 {
     constructor() {
         this.objects = [];
-        this.collisionIndex = [];
-
         this.garbage = [];
-
         this.collisionMaxDistanceSq = undefined;
     }
 
@@ -70,18 +68,6 @@ class Collision
             throw new TypeError('Collidable wrong type');
         }
         this.objects.push(new Entry(object));
-        this.collisionIndex.push([]);
-    }
-
-    garbageCollect() {
-        let object;
-        let index;
-        while (object = this.garbage.pop()) {
-            while ((index = this.objects.findIndex(entry => entry.entity === object)) !== -1) {
-                this.objects.splice(index, 1);
-                this.collisionIndex.splice(index, 1);
-           }
-        }
     }
 
     removeObject(object) {
@@ -91,69 +77,40 @@ class Collision
     detect() {
         this.garbageCollect();
 
-        for (let i = 0, l = this.objects.length; i !== l; ++i) {
-            if (this.objects[i].needsCheck()) {
-                for (let j = 0; j !== l; ++j) {
-                    if (i !== j && this.objects[j].entity.collidable) {
-                        this.objectIndexesCollide(i, j);
+        this.objects.forEach(entry1 => {
+            if (entry1.needsCheck()) {
+                this.objects.forEach(entry2 => {
+                    if (entry1 !== entry2 && entry2.entity.collidable) {
+                        this.objectsCollide(entry1, entry2);
                     }
-                }
+                });
             }
-        }
+        });
 
-        for (let i = 0, l = this.objects.length; i !== l; ++i) {
-            this.objects[i].updateLastPos();
+        this.objects.forEach(entry => entry.updateLastPos());
+    }
+
+    garbageCollect() {
+        let object;
+        let index;
+        while (object = this.garbage.pop()) {
+            while ((index = this.objects.findIndex(entry => entry.entity === object)) !== -1) {
+                this.objects.splice(index, 1);
+           }
         }
     }
 
-    objectIndexesCollide(i, j) {
-        const o1 = this.objects[i].entity;
-        const o2 = this.objects[j].entity;
-
-        if (this.objectsCollide(o1, o2)) {
-            if (this.collisionIndex[i].indexOf(o2) < 0) {
-                this.collisionIndex[i].push(o2);
-            }
-            return true;
-        }
-        else {
-            const ix = this.collisionIndex[i].indexOf(o2);
-            if (ix !== -1) {
-                o1.uncollides(o2);
-                o2.uncollides(o1);
-                this.collisionIndex[i].splice(ix, 1);
-            }
-            return false;
-        }
-    }
-
-    objectsCollide(o1, o2) {
-        if (o1.position.distanceToSquared(o2.position) > this.collisionMaxDistanceSq) {
+    objectsCollide(entry1, entry2) {
+        if (entry1.entity.position.distanceToSquared(
+            entry2.entity.position) > this.collisionMaxDistanceSq) {
             return false;
         }
 
-        for (let i = 0, l = o1.collision.length; i !== l; ++i) {
-            const z1 = o1.collision[i];
-            for (let j = 0, m = o2.collision.length; j !== m; ++j) {
-                const z2 = o2.collision[j];
-                if (this.zonesCollide(o1, z1, o2, z2)) {
-                    o1.collides(o2, z1, z2);
-                    o2.collides(o1, z2, z1);
-                    return true;
-                }
-            }
-        }
-        return false;
+        return entry1.check(entry2);
     }
 
     setCollisionRadius(units) {
         this.collisionMaxDistanceSq = units * units;
-    }
-
-    zonesCollide(object1, zone1, object2, zone2) {
-        return rectanglesIntersect(
-            zone1.x, zone1.y, zone1.w, zone1.h,
-            zone2.x, zone2.y, zone2.w, zone2.h);
     }
 }
 
