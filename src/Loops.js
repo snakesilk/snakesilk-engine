b05e0e20e5fc48ae190de743c793a535c1863d4c
Pontus Persson <pontus.alexander@gmail.com>
2017-08-13 09:33:11 +0200

    Modernize Loops.

--- src/Loops.js
+++ src/Loops.js
@@ -1,48 +1,49 @@
 const SyncPromise = require('./SyncPromise');
 
-const Loops = {
-    doFor: function(events, event) {
-        return function doFor(duration, callback) {
-            if (duration <= 0) {
-                if (callback) {
-                    callback(0, 1);
-                }
-                return SyncPromise.resolve();
+function doFor(events, event) {
+    return function doFor(duration, callback) {
+        if (duration <= 0) {
+            if (callback) {
+                callback(0, 1);
             }
+            return SyncPromise.resolve();
+        }
 
-            let elapsed = 0;
-            let progress = 0;
-            return new SyncPromise(resolve => {
-                function doForWrapper(dt, total, tick) {
-                    elapsed += dt;
-                    progress = elapsed / duration;
-                    if (progress >= 1) {
-                        progress = 1;
-                        events.unbind(event, doForWrapper);
-                    }
-                    if (callback) {
-                        callback(elapsed, progress);
-                    }
-                    if (progress === 1) {
-                        resolve({
-                            elapsed,
-                            tick,
-                            offset: elapsed - duration,
-                            total: total,
-                        });
-                    }
+        let elapsed = 0;
+        let progress = 0;
+        return new SyncPromise(resolve => {
+            function doForWrapper(dt, total, tick) {
+                elapsed += dt;
+                progress = elapsed / duration;
+                if (progress >= 1) {
+                    progress = 1;
+                    events.unbind(event, doForWrapper);
                 }
-                events.bind(event, doForWrapper);
-            });
-        }
-    },
-    waitFor: function(events, event) {
-        const doFor = Loops.doFor(events, event);
-        return function waitFor(seconds) {
-            return doFor(seconds);
-        }
-    },
+                if (callback) {
+                    callback(elapsed, progress);
+                }
+                if (progress === 1) {
+                    resolve({
+                        elapsed,
+                        tick,
+                        offset: elapsed - duration,
+                        total: total,
+                    });
+                }
+            }
+            events.bind(event, doForWrapper);
+        });
+    };
 }
 
-module.exports = Loops;
+function waitFor(events, event) {
+    const doForWrapper = doFor(events, event);
+    return function waitFor(seconds) {
+        return doForWrapper(seconds);
+    };
+}
 
+module.exports = {
+    doFor,
+    waitFor,
+};
